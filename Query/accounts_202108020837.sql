d57b06f72e3537cee4905e7244fa29f57a1b8112
zyon-revature <86679832+zyon-revature@users.noreply.github.com>
2021-08-02 08:40:43 -0500

    Add files via upload

---
+++ Query/accounts_202108020837.sql
@@ -0,0 +1,6 @@
+INSERT INTO bank.accounts (balance) VALUES
+	 (7500.0),
+	 (250.0),
+	 (3299.0),
+	 (295.0),
+	 (8735.0);
---
+++ Query/customers_202108020838.sql
@@ -0,0 +1,6 @@
+INSERT INTO bank.customers (f_name,l_name,username,userpassword) VALUES
+	 ('Zyon','Savery','z.savery','OpGR1970'),
+	 ('Nick','Garcia','nick.garcia','H?4XhGh34'),
+	 ('Abby','Davis','aabbDavis','kBEQfq!97x'),
+	 ('Danny','Fenton','Ghost1789','Ka8nZJr_TV'),
+	 ('Sara','West','sWest521','L3LA&m9$wE');
